18bee56cd5ba0e09b397b5753ffe819a48688e74
Prince7g <princeraj72g5@gmail.com>
2025-06-25 10:03:30 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Home from './pages/Home';
 import ProductDetail from './pages/ProductDetail';
 import Cart from './pages/Cart';
+import MagentoDemo from './components/MagentoDemo'; // Bonus demo route (not linked)
 
 export default function App() {
   return (
@@ -12,6 +13,8 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/product/:id" element={<ProductDetail />} />
         <Route path="/cart" element={<Cart />} />
+        {/* Hidden Magento demo route (for bonus only) */}
+        <Route path="/magento-demo" element={<MagentoDemo />} />
       </Routes>
     </div>
   );
