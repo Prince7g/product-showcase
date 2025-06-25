270994308d88b1357c0df89e4cc4f571b419ed71
Prince7g <princeraj72g5@gmail.com>
2025-06-25 09:06:40 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './pages/Home';
+import ProductDetail from './pages/ProductDetail';
+import Cart from './pages/Cart';
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </div>
+  );
+}
