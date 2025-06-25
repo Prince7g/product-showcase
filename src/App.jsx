2c91bb64f682bc539fea0ce2352fba12372eb39a
Prince7g <princeraj72g5@gmail.com>
2025-06-25 11:10:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
-import Home from './pages/Home';
-import ProductDetail from './pages/ProductDetail';
-import Cart from './pages/Cart';
-import MagentoDemo from './components/MagentoDemo'; // Bonus demo route (not linked)
+import routes from './routes';
 
 export default function App() {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/product/:id" element={<ProductDetail />} />
-        <Route path="/cart" element={<Cart />} />
-        {/* Hidden Magento demo route (for bonus only) */}
-        <Route path="/magento-demo" element={<MagentoDemo />} />
+        {routes.map(({ path, element }, index) => (
+          <Route key={index} path={path} element={element} />
+        ))}
       </Routes>
     </div>
   );
